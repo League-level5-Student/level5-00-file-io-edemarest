5f7e006bc260daf7e98e63487e54e79ecba76d09
edemarest <Lunacat7!>
2020-04-30 19:25:21 -0700

    4/30/20

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,23 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
+	public static void main(String[] args) {
+		String message = JOptionPane.showInputDialog("Enter a message:");
+		
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/test2.txt");
+
+			fw.write(message);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	// Create a program that takes a message from the user and saves it to a file.
 }
+
